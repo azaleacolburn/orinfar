719e4115574b940804090c37f10268d4ce1a7687
Azalea Colburn <azaleacolburn@gmail.com>
2025-07-08 22:19:34 -0700

    feat: basic text editing and the first few vim commands

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::io::stdout;
+use std::io::{stdout, Write};
 
 use crossterm::{
     cursor::{
-        self, position, MoveDown, MoveLeft, MoveRight, MoveTo, MoveUp, RestorePosition,
-        SavePosition,
+        self, position, DisableBlinking, EnableBlinking, MoveDown, MoveLeft, MoveRight, MoveTo,
+        MoveToColumn, MoveUp, RestorePosition, SavePosition,
     },
     event::{read, Event, KeyCode},
     execute,
@@ -13,6 +13,12 @@ use crossterm::{
     },
     ExecutableCommand,
 };
+
+#[derive(Clone, Debug)]
+struct Cursor {
+    row: usize,
+    col: usize,
+}
 
 #[derive(Clone, Debug)]
 enum Mode {
@@ -30,28 +36,47 @@ fn main() -> std::io::Result<()> {
         Clear(ClearType::All),
         ScrollUp(rows),
         SetForegroundColor(Color::Blue),
-        // SetBackgroundColor(Color::Red),
-        Print("Hello World"),
+        // SetBackgroundColor(Color::DarkGrey),
     )?;
 
     // Fill entire screen with spaces with the background color
     for row in 0..rows {
         execute!(stdout(), MoveTo(0, row), Print(" ".repeat(cols as usize)))?;
     }
-    execute!(stdout(), MoveTo(0, rows))?;
+    execute!(stdout(), MoveTo(0, 0))?;
 
     enable_raw_mode()?;
+    let mut stdout = stdout();
+
+    let mut buffer: Vec<Vec<char>> = vec![vec![]];
+    let mut cursor = Cursor { row: 0, col: 0 };
 
     let mut mode = Mode::Normal;
 
     let mut count: u16 = 1;
+    let mut chained: Vec<KeyCode> = vec![];
 
     loop {
         if let Event::Key(event) = read()? {
             match (event.code, mode.clone()) {
-                (KeyCode::Char(c), Mode::Normal) if c == 'i' => mode = Mode::Insert,
-                (KeyCode::Char(c), Mode::Normal) if c == 'm' => {
-                    stdout().execute(Print(format!("{:?}", mode)))?;
+                (KeyCode::Char(c), Mode::Normal) if c == 'i' => {
+                    mode = Mode::Insert;
+                    execute!(stdout, EnableBlinking)?;
+                }
+                (KeyCode::Char(c), Mode::Normal) if c == '$' => {
+                    cursor.col = buffer[cursor.row].len()
+                }
+                (KeyCode::Char(c), Mode::Normal) if c == '_' => {
+                    let first = buffer[cursor.row]
+                        .iter()
+                        .position(|c| !c.is_whitespace())
+                        .unwrap_or(cursor.col);
+                    cursor.col = first
+                }
+                (KeyCode::Char(c), Mode::Normal) if c == 'x' => {
+                    if buffer[cursor.row].len() > cursor.col {
+                        buffer[cursor.row].remove(cursor.col);
+                    }
                 }
                 (KeyCode::Char(c), Mode::Normal) if c == 'q' => break,
                 (KeyCode::Char(c), Mode::Normal) if c.is_numeric() => {
@@ -61,38 +86,82 @@ fn main() -> std::io::Result<()> {
                     }
                     count *= 10;
                     count += c;
-                    stdout().execute(Print(format!("{:?}\n", count)))?;
                 }
 
                 (KeyCode::Esc, Mode::Insert) => {
                     mode = Mode::Normal;
+                    execute!(stdout, DisableBlinking)?;
                     count = 1;
                 }
                 (KeyCode::Backspace, Mode::Insert) => {
-                    execute!(stdout(), MoveLeft(2), Print(" "), MoveRight(1))?;
+                    if cursor.col > 0 {
+                        buffer[cursor.row].remove(cursor.col - 1);
+                        cursor.col -= 1;
+                    } else if cursor.row != 0 {
+                        let mut old_line = buffer[cursor.row].clone();
+                        buffer[cursor.row - 1].append(&mut old_line);
+                        buffer.remove(cursor.row);
+                        cursor.row -= 1;
+                        cursor.col = buffer[cursor.row].len()
+                    }
                 }
                 (KeyCode::Char(c), Mode::Insert) => {
-                    stdout().execute(Print(c))?;
+                    buffer[cursor.row].insert(cursor.col, c);
+                    cursor.col += 1;
+                }
+                (KeyCode::Enter, Mode::Insert) => {
+                    let end = if buffer[cursor.row].len() > 0 {
+                        buffer[cursor.row].split_off(cursor.col)
+                    } else {
+                        vec![]
+                    };
+
+                    buffer.push(end);
+                    cursor.col = 0;
+                    cursor.row += 1;
                 }
 
                 (KeyCode::Left, _) => {
-                    stdout().execute(MoveLeft(count))?;
+                    if cursor.col > 0 {
+                        cursor.col -= 1;
+                    }
                 }
                 (KeyCode::Right, _) => {
-                    stdout().execute(MoveRight(count))?;
+                    if cursor.col < buffer[cursor.row].len() {
+                        cursor.col += 1;
+                    }
                 }
                 (KeyCode::Up, _) => {
-                    stdout().execute(MoveUp(count))?;
+                    if cursor.row > 0 {
+                        cursor.row -= 1;
+                        // The cursor can exist one character beyond the last in the buffer
+                        cursor.col = usize::min(cursor.col, buffer[cursor.row].len())
+                    }
                 }
                 (KeyCode::Down, _) => {
-                    stdout().execute(MoveDown(count))?;
+                    if cursor.row < buffer.len() - 1 {
+                        cursor.row += 1;
+                        cursor.col = usize::min(cursor.col, buffer[cursor.row].len())
+                    }
                 }
                 _ => continue,
             };
+
+            execute!(stdout, MoveTo(0, 0), Clear(ClearType::All),)?;
+            for row in buffer.iter() {
+                execute!(
+                    stdout,
+                    Print(row.clone().into_iter().collect::<String>()),
+                    MoveDown(1),
+                    MoveToColumn(0),
+                )?;
+            }
+            execute!(stdout, MoveTo(cursor.col as u16, cursor.row as u16))?;
+            stdout.flush()?;
         }
     }
 
-    execute!(stdout(), SetSize(cols, rows), ResetColor)?;
+    execute!(stdout, SetSize(cols, rows), ResetColor)?;
     disable_raw_mode()?;
 
     Ok(())
