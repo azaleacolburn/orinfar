a09933fbc54ad9f9904e14e37384415dd68e59ac
Azalea Colburn <azaleacolburn@gmail.com>
2025-07-13 15:24:27 -0700

    feat: command abstraction

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ use crossterm::{
     ExecutableCommand,
 };
 
+use crate::commands::{b_cmd, dd_cmd, dollar_cmd, o_cmd, underscore_cmd, w_cmd, x_cmd, O_cmd};
+
 #[derive(Clone, Debug)]
 struct Cursor {
     row: usize,
@@ -57,49 +59,15 @@ fn main() -> std::io::Result<()> {
     let mut buffer: Vec<Vec<char>> = vec![vec![]];
     let mut cursor = Cursor { row: 0, col: 0 };
 
-    let mut commands = vec![
-        Cmd::normal("w", || {
-            let mut next_char = if cursor.col + 1 != buffer[cursor.row].len() {
-                buffer[cursor.row][cursor.col + 1]
-            } else if cursor.row + 1 != buffer.len() {
-                buffer[cursor.row + 1][0]
-            } else {
-                // Functionally aborts the callback
-                buffer[cursor.row][cursor.col]
-            };
-            while next_char.is_alphanumeric() {
-                if cursor.col + 1 != buffer[cursor.row].len() {
-                    cursor.col += 1;
-                } else if cursor.row + 1 != buffer.len() {
-                    cursor.row += 1;
-                    cursor.col = 0;
-                } else {
-                    break;
-                }
-                next_char = buffer[cursor.row][cursor.col];
-            }
-        }),
-        Cmd::normal("b", || {
-            let mut next_char = if cursor.col != 0 {
-                buffer[cursor.row][cursor.col - 1]
-            } else if cursor.row != 0 {
-                buffer[cursor.row - 1][buffer[cursor.row - 1].len()]
-            } else {
-                // Functionally aborts the callback
-                buffer[cursor.row][cursor.col]
-            };
-            while next_char.is_alphanumeric() {
-                if cursor.col != 0 {
-                    cursor.col -= 1;
-                } else if cursor.row != 0 {
-                    cursor.row -= 1;
-                    cursor.col = buffer[cursor.row].len();
-                } else {
-                    break;
-                }
-                next_char = buffer[cursor.row][cursor.col];
-            }
-        }),
+    let commands = vec![
+        Cmd::new("w", w_cmd),
+        Cmd::new("b", b_cmd),
+        Cmd::new("$", dollar_cmd),
+        Cmd::new("_", underscore_cmd),
+        Cmd::new("x", x_cmd),
+        Cmd::new("o", o_cmd),
+        Cmd::new("O", O_cmd),
+        Cmd::new("dd", dd_cmd),
     ];
 
     let mut mode = Mode::Normal;
@@ -114,50 +82,6 @@ fn main() -> std::io::Result<()> {
                     mode = Mode::Insert;
                     execute!(stdout, EnableBlinking)?;
                 }
-                (KeyCode::Char(c), Mode::Normal) if c == '$' => {
-                    cursor.col = buffer[cursor.row].len()
-                }
-                (KeyCode::Char(c), Mode::Normal) if c == '_' => {
-                    let first = buffer[cursor.row]
-                        .iter()
-                        .position(|c| !c.is_whitespace())
-                        .unwrap_or(cursor.col);
-                    cursor.col = first
-                }
-                (KeyCode::Char(c), Mode::Normal) if c == 'x' => {
-                    if buffer[cursor.row].len() > cursor.col {
-                        buffer[cursor.row].remove(cursor.col);
-                    }
-                }
-                (KeyCode::Char(c), Mode::Normal) if c == 'o' => {
-                    cursor.row += 1;
-                    buffer.insert(cursor.row, vec![]);
-                    cursor.col = 0;
-                    mode = Mode::Insert;
-                }
-                (KeyCode::Char(c), Mode::Normal) if c == 'O' => {
-                    if cursor.row > 0 {
-                        buffer.insert(cursor.row, vec![]);
-                        cursor.col = 0;
-                        mode = Mode::Insert;
-                    }
-                }
-                (KeyCode::Char(c), Mode::Normal)
-                    if c == 'd' && chained.len() > 0 && chained[0] == 'd' =>
-                {
-                    if buffer.len() > 1 {
-                        buffer.remove(cursor.row);
-                        if cursor.row == buffer.len() {
-                            cursor.row -= 1;
-                        }
-                    } else {
-                        buffer[0] = vec![];
-                        cursor.col = 0;
-                    }
-                    chained = vec![]
-                }
-                (KeyCode::Char(c), Mode::Normal) if c == 'w' => {}
-                (KeyCode::Char(c), Mode::Normal) if c == 'b' => {}
                 (KeyCode::Char(c), Mode::Normal) if c == 'q' => break,
                 (KeyCode::Char(c), Mode::Normal) if c.is_numeric() => {
                     let c = c.to_digit(10).unwrap() as u16;
@@ -167,7 +91,15 @@ fn main() -> std::io::Result<()> {
                     count *= 10;
                     count += c;
                 }
-                (KeyCode::Char(c), Mode::Normal) => chained.push(c),
+                (KeyCode::Char(c), Mode::Normal) => {
+                    chained.push(c);
+                    let mut matched_list = commands.iter().filter(|cmd| chained == cmd.chain);
+                    if let Some(matched) = matched_list.next() {
+                        assert!(matched_list.next().is_none());
+                        (matched.callback)(&mut buffer, &mut cursor, &mut mode);
+                        chained = vec![];
+                    }
+                }
 
                 (KeyCode::Esc, Mode::Insert) => {
                     mode = Mode::Normal;
