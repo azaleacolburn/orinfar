4f1133dee622b8eedd477c659261e7f08413fed7
Azalea Colburn <azaleacolburn@gmail.com>
2025-07-28 20:05:35 -0700

    feat: dd yanks and p pastes

--- src/register.rs
+++ src/register.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 pub type RegId = String;
-pub type RegContents = String;
+pub type RegContents = Vec<char>;
 pub type Registers = HashMap<RegId, RegContents>;
 
 pub struct RegisterHandler {
@@ -17,9 +17,21 @@ impl RegisterHandler {
         }
     }
 
-    pub fn init_reg(&mut self, reg: impl ToString, value: impl ToString) {
+    pub fn init_reg(&mut self, reg: impl ToString, value: RegContents) {
+        self.registers.entry(reg.to_string()).or_insert(value);
+    }
+
+    pub fn set_reg(&mut self, value: RegContents) {
+        self.registers.insert(self.current_register.clone(), value);
+    }
+    pub fn get_reg(&mut self) -> RegContents {
         self.registers
-            .entry(reg.to_string())
-            .or_insert(value.to_string());
+            .get(&self.current_register)
+            .unwrap_or(&Vec::new())
+            .clone()
+    }
+
+    pub fn reset_current_register(&mut self) {
+        self.current_register = "0".into()
     }
 }
