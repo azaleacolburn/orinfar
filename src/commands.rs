8ccb021a130a4a2dad40f4210f5c99d8068f4415
Azalea Colburn <azaleacolburn@gmail.com>
2026-01-02 15:32:28 -0800

    fix: still keep track of instances

--- src/commands.rs
+++ src/commands.rs
@@ -190,7 +190,7 @@ pub fn replace(
         let original_char = buffer.get_curr_char();
         buffer.replace_curr_char(c);
 
-        let action = Action::replace(original_char, c);
+        let action = Action::replace(vec![buffer.cursor], original_char, c);
         undo_tree.new_action_merge(action);
     }
     execute!(stdout(), SetCursorStyle::SteadyBlock).unwrap();
